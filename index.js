080026df0b1e3b2af4669e59165ab93d4c1cbdff
Al3xi3w <156727387+al3xi3w@users.noreply.github.com>
2025-09-06 12:41:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -105,6 +105,10 @@ app.delete("/tasks/:id", (req, res) => {
 });
 
 /* ---------------------- ARRANQUE ---------------------- */
-app.listen(PORT, () => {
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
+
