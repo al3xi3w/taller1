d0dc7b7a7ec25184dda91722e0f155cc67bcd8e1
Al3xi3w <156727387+al3xi3w@users.noreply.github.com>
2025-09-06 13:43:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
-const PORT = process.env.PORT || 3000;   // ✅ Definido solo una vez
+const PORT = process.env.PORT || 8000;   
 
 // Middleware para parsear JSON
 app.use(express.json());
